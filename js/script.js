25b1e2db7dca299f65aa943968e9ff4bf674c3b8
MarcoB <mrc.bsllt@gmail.com>
2020-12-18 15:33:54 +0100

    copiato l'array chiamato nei miei data, ma non funziona il v-for

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 // 6aec7bf32e62af91512f360891825035 <-- chiave api
+var app = new Vue (
+  {
+    el: "#app",
+    data: {
+      films: []
+    }, //fine data
+
+    methods: {
+      searchFilms: function() {
+
+        const self = this;
+
+        axios
+          .get("https://api.themoviedb.org/3/search/movie", {
+            params: {
+              api_key: "6aec7bf32e62af91512f360891825035",
+              query: "ritorno al fut",
+              language: "it-IT"
+            }
+          })
+          .then(function (response) {
+              this.films = response.data.results;
+              console.log(this.films);
+            }
+          )
+
+
+      }
+    } //fine methods
+
+  } //fine istanza vue
+);
